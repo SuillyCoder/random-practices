2699afb9f6878464df141ce25d40992d4b51fe04
Joshua Sarcol <jjsnippets@gmail.com>
2024-07-11 14:52:16 +0800

    filled array with user input

--- 6-2d-sorting.c
+++ 6-2d-sorting.c
@@ -14,33 +14,64 @@ start: 13:38
 
 
 typedef struct coord {
-    int x;
-    int y;
+    int row;
+    int col;
 } COORD;
+
+void arrayDisp(int** arr, COORD dims){
+    for (int i = 0; i < dims.row; i++){
+        for(int j = 0; j < dims.col; j++){
+            printf("%2d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 int main() {
     COORD dims;
     char* input = calloc(250, sizeof(char));
-    int maxElements;
 
+    // user prompt
     printf("START!\n");
     scanf("%[^\n]249s", input);
 
-    dims.x = atoi(strtok(input, " "));
-    dims.y = atoi(strtok(NULL, " "));
-    unsigned int totalElements = dims.x * dims.y;
-    printf("%d\n", totalElements);
+    // dimensions of array: x by y grid
+    dims.col = atoi(strtok(input, " "));
+    dims.row = atoi(strtok(NULL, " "));
+    int totalElements = dims.row * dims.col;
+    // printf("%d\n", totalElements);
 
-    int* raw = calloc(totalElements, sizeof(int));
+    // solution array
+    int** solution = calloc(dims.row, sizeof(int*));
+    for(int i = 0; i < dims.row; i++)
+        solution[i] = calloc(dims.col, sizeof(int));
 
-    for(int i = 0; i < totalElements; i++){
-        raw[i] = atoi(strtok(NULL, " "));
+    // filling the array with user values
+    for(int i = 0, x = 0, y = 0; i < totalElements; i++){
+        
+        if(x >= dims.col){
+            x = 0;
+            y++;
+        }
+
+        solution[y][x++] = atoi(strtok(NULL, " "));
     }
 
-    for(int i = 0; i < totalElements; i++){
-        printf("%d ", raw[i]);
-    }
+    // int* raw = calloc(totalElements, sizeof(int));
 
-    printf("\n\nDone!\n");
+    // for(int i = 0; i < totalElements; i++){
+    //     raw[i] = atoi(strtok(NULL, " "));
+    // }
+
+    // for(int i = 0; i < totalElements; i++){
+    //     printf("%d ", raw[i]);
+    // }
+
+    // printf("\n\n I/O done!\n");
+
+    arrayDisp(solution, dims);
+
+    printf("\n\n display done!\n");
+    
 
 }
