9f0d64fcb0feb5b6f9fe57576a1423094d944c22
Enzo Basuil <146545169+SuillyCoder@users.noreply.github.com>
2024-09-20 11:04:32 +0800

    Update Enzo.c

--- Enzo.c
+++ Enzo.c
@@ -3,6 +3,12 @@
 int main{
   printf("Hello World!");
   printf("New branch");
-	printf("Conflict test");
+  printf("Conflict test");
+
+  // This is code for branch 1
+   int x = 4;
+   int y = 10;
+
+   printf("The sum of x and y is: %d", (x + y));
   return 0;
 }
